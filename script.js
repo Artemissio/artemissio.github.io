26728bf5eed9413a54c3ed0610e4f2e4034ad3dd
Artemissio <artemissterio@gmail.com>
2021-02-18 22:07:42 +0200

    Changed menu logic
    Changed menu logic

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-var previousLink;
-var previousMenu;
-var previousSubMenu;
 var previousParagraph;
-var previousSection;
 var previousTable;
 
 const flex = "flex";
@@ -27,109 +23,114 @@ const invisibleTableClass = "table-invisible";
 const visibleSubMenuClass = "sub-menu-visible";
 const activeParagraphClass = "active-paragraph";
 
+function GetPreviousSubmenu(){
+    return document.getElementById(menuClass).getElementsByClassName(subMenuClass + ' ' + activeLinkClass)[0];
+}
+
 function GetPreviousMenu(){
-    let result = document.getElementsByClassName(menuItemClass + ' ' + activeLinkClass)[0];
+    return document.getElementById(menuClass).getElementsByClassName(menuItemClass + ' ' + activeLinkClass)[0];
+}
 
-    if(result === undefined){
-        result = document.getElementsByClassName(subMenuClass + ' ' + activeLinkClass)[0];
-    }
-
-    return result;
+function GetPreviousSubmenu(menu){
+    return menu.parentNode.getElementsByClassName(subMenuClass + ' ' + activeLinkClass)[0];
 }
 
 function ChangeContent(menu, sectionID, paragraphID = null){
-    if(previousMenu === undefined){
-        previousMenu = GetPreviousMenu();        
-    }
+    let previousMenu = GetPreviousMenu();
 
-    let burgerMenu = document.getElementById(burgerMenuClass).children[0];
-    if(burgerMenu.innerText === closeClass){
-        showOrHideMenu();
+    let hasPreviousSubmenus = !previousMenu.classList.contains(noSubmenusClass);
+    
+    let previousSubMenu;
+
+    if(hasPreviousSubmenus){
+        previousSubMenu = GetPreviousSubmenu(previousMenu);
     }
+    else{
+        previousSubMenu = undefined;
+    }
+    
+    let hasSubmenus = !menu.classList.contains(noSubmenusClass);
 
     switch(menu.classList[0]){
         case menuItemClass:
             console.log(menuItemClass);
 
-            if(menu === previousMenu) return;
+            if(menu === previousMenu){
+                if(hasPreviousSubmenus){
+                    ShowOrHideMenus(previousMenu);
+                    ChangeMenuIcon(previousMenu);
+                }
+                return;
+            }
 
-            DisableActiveMenus();
-
-            menu.classList.add(activeLinkClass);
-            previousMenu.classList.remove(activeLinkClass);
-
-            if(!previousMenu.classList.contains(noSubmenusClass)){
+            if(hasPreviousSubmenus){
                 previousMenu.childNodes[1].innerText = "add";
                 HideSubMenus(previousMenu);
             }
 
-            if(!menu.classList.contains(noSubmenusClass)){
+            DisableActiveMenus();
+            menu.classList.add(activeLinkClass);
+            previousMenu.classList.remove(activeLinkClass);
+
+            if(hasSubmenus){
                 ShowOrHideMenus(menu)
                 ChangeMenuIcon(menu);
             }
             else{
-                let activeItem = document.getElementById(contentClass).getElementsByClassName(activeItemClass)[0];
-                let item = document.getElementById(sectionID);
+                ChangeSection(sectionID);
 
-                activeItem?.classList.replace(activeItemClass, itemClass);
-                activeItem?.classList.remove(flex);
-
-                item.classList.replace(itemClass, activeItemClass);
-                item?.classList.add(flex);
+                let burgerMenu = document.getElementById(burgerMenuClass).children[0];
+                if(burgerMenu.innerText === closeClass){
+                    showOrHideMenu();
+                }
             }
-            
             break;
-
+    
         case subMenuClass:
             console.log(subMenuClass);
 
-            let parent = menu.parentNode.children[0];
+            if(menu === previousSubMenu) return;
 
-            if(previousMenu === parent){
-                previousMenu = document.getElementsByClassName(subMenuClass + ' ' + visibleSubMenuClass + ' ' + activeLinkClass)[0];
-            }
-
-            if(menu === previousMenu) return;   
-            
-            console.log({previousMenu});
-            
+            previousSubMenu?.classList.remove(activeLinkClass);
             menu.classList.add(activeLinkClass);
-            previousMenu.classList.remove(activeLinkClass);
 
-            if(!previousMenu.classList.contains(noSubmenusClass)){
-                previousMenu.childNodes[1].innerText = "add";
-                HideSubMenus(previousMenu);
-                previousMenu.classList.remove(activeLinkClass);
-            }
-            else{
+            ChangeSection(sectionID);
 
-            }
-            
-            // HideSubMenus(previousMenu);
-            // ShowOrHideMenus(menu);
+            if(previousParagraph !== paragraphID){
+                let oldParagraph = document.getElementById(previousParagraph);
 
-            // if(paragraphID !== null && previousParagraph !== paragraphID){
+                if(oldParagraph === null){
+                    oldParagraph = document.getElementById("content").getElementsByClassName(activeParagraphClass)[0];
+                }
 
-            //     if(previousParagraph === undefined){
-            //         previousParagraph = document.getElementsByClassName(activeParagraphClass)[0].id;
-            //         console.log(previousParagraph);
-            //     }
-
-            //     let oldParagraph = document.getElementById(previousParagraph);
-            //     let newParagraph = document.getElementById(paragraphID);
+                let newParagraph = document.getElementById(paragraphID);
         
-            //     oldParagraph.classList.remove(activeParagraphClass);
-            //     newParagraph.classList.add(activeParagraphClass);
+                oldParagraph.classList.remove(activeParagraphClass);
+                newParagraph.classList.add(activeParagraphClass);
         
-            //     previousParagraph = paragraphID;
-            // }
-            
+                previousParagraph = paragraphID;
+            }
+
+            let burgerMenu = document.getElementById(burgerMenuClass).children[0];
+            if(burgerMenu.innerText === closeClass){
+                showOrHideMenu();
+            }
             break;
 
-        default: break;
+        default: 
+            break;
     }
+}
 
-    previousMenu = menu;
+function ChangeSection(sectionID) {
+    let activeItem = document.getElementById(contentClass).getElementsByClassName(activeItemClass)[0];
+    let item = document.getElementById(sectionID);
+
+    activeItem?.classList.replace(activeItemClass, itemClass);
+    activeItem?.classList.remove(flex);
+
+    item.classList.replace(itemClass, activeItemClass);
+    item?.classList.add(flex);
 }
 
 function DisableActiveMenus(){
@@ -146,29 +147,7 @@ function HideSubMenus(menu){
     for(let i = 0; i < parentDivChilds.length; i++){
         let subMenuClasses = parentDivChilds[i].classList;
 
-        subMenuClasses.remove(visibleSubMenuClass);
-    }
-}
-
-function ShowSubmenus(menu){
-    ChangeMenuIcon(menu);
-
-    if(previousMenu === undefined){
-        previousMenu = document.getElementById(menuClass).getElementsByClassName(activeLinkClass)[0];
-    }
-
-    if(menu === previousMenu){
-        ShowOrHideMenus(previousMenu);
-    }
-    else{
-        HideSubMenus(previousMenu);
-        ShowOrHideMenus(menu);
-
-        previousMenu.classList.remove(activeLinkClass);
-        menu.classList.add(activeLinkClass);
-        
-        previousMenu.childNodes[1].innerText = "add";
-        previousMenu = menu;
+        subMenuClasses.remove(visibleSubMenuClass, activeLinkClass);
     }
 }
 
@@ -190,98 +169,6 @@ function ShowOrHideMenus(menu){
             subMenuClasses.add(visibleSubMenuClass);
         }
     }
-}
-
-function ShowSection(subMenu, sectionID, paragraphID){
-    if(previousSubMenu === subMenu || previousParagraph === paragraphID) return;
-
-    if(previousSubMenu === undefined){
-        previousSubMenu = GetPreviousSubmenu();
-    }
-
-    if(previousSection === undefined){
-        previousSection = GetPreviousSection().id;
-    }
-
-    if(previousParagraph === undefined){
-        previousParagraph = GetPreviousParagraph().id;
-    }
-
-    previousSubMenu.classList.remove(activeLinkClass);
-
-    previousSubMenu = subMenu;
-
-    if(previousSection !== sectionID){
-        let activeItem = document.getElementById(contentClass).getElementsByClassName(activeItemClass)[0];
-        let item = document.getElementById(sectionID);
-    
-        activeItem?.classList.replace(activeItemClass, itemClass);
-        activeItem?.classList.remove(flex);
-    
-        item.classList.replace(itemClass, activeItemClass);
-        item?.classList.add(flex);
-
-        previousSection = sectionID;
-    }
-
-    if(previousParagraph !== paragraphID){
-        let oldParagraph = document.getElementById(previousParagraph);
-        let newParagraph = document.getElementById(paragraphID);
-
-        oldParagraph.classList.remove(activeParagraphClass);
-        newParagraph.classList.add(activeParagraphClass);
-
-        previousParagraph = paragraphID;
-    }
-
-    let burgerMenu = document.getElementById(burgerMenuClass).children[0];
-    if(burgerMenu.innerText === closeClass){
-        showOrHideMenu();
-    }   
-
-    if(!subMenu.classList.contains(activeLinkClass)){
-        subMenu.classList.add(activeLinkClass);
-    }
-}
-
-function GetPreviousSubmenu(){
-    return document.getElementsByClassName(subMenuClass + ' ' + activeLinkClass)[0];
-}
-
-function GetPreviousSection(){
-    return document.getElementsByClassName(activeItemClass)[0];
-}
-
-function GetPreviousParagraph(){
-    return document.getElementsByClassName(activeParagraphClass)[0];
-}
-
-function ChangeTab(menu, sectionID){
-    if (previousMenu === menu) return;
-    
-    previousMenu = menu;
-
-    let burgerMenu = document.getElementById(burgerMenuClass).children[0];
-    if(burgerMenu.innerText === closeClass){
-        showOrHideMenu();
-    }    
-
-    let activeItem = document.getElementById(contentClass).getElementsByClassName(activeItemClass)[0];
-    let item = document.getElementById(sectionID);
-
-    activeItem?.classList.replace(activeItemClass, itemClass);
-    activeItem?.classList.remove(flex);
-
-    item.classList.replace(itemClass, activeItemClass);
-    item?.classList.add(flex);
-
-    let activeLinks = document.getElementsByClassName(activeLinkClass);
-
-    for(let i = 0; i < activeLinks.length; i++){
-        activeLinks[i].classList.remove(activeLinkClass);
-    }
-
-    menu.classList.add(activeLinkClass);
 }
 
 function showOrHideMenu(){
